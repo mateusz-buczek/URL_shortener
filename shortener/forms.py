e8298d84403ff8a541e883bd74eab1b2e9b3f497
mateusz-buczek <mateusz.buczek89@gmail.com>
2019-03-28 18:01:04 +0100

    fixed forms widgets

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.utils.translation import gettext_lazy as _
 from .models import Addresses
 
 
@@ -7,13 +7,13 @@ class AddressesForm(forms.ModelForm):
     class Meta:
         model = Addresses
         fields = ('original', 'shortened')
-        # help_texts = {
-        #     'original': _('Paste URL here'),
-        #     'shortened': _('custom short name(optional)'),
-        # }
-        # error_messages = {
-        #     'shortened': {
-        #         'max_length': _('Too long(30 chars limit)'),
-        #     }
-        # }
+        help_texts = {
+            'original': _('Paste URL here'),
+            'shortened': _('custom short name(optional)'),
+        }
+        error_messages = {
+            'shortened': {
+                'max_length': _('Too long(30 chars limit)'),
+            }
+        }
 # TODO forms with validations, hook it up to html
